e4c39173ed6ba49aa065d745940fa014e338cc61
divyanshu1625 <165761744+divyanshu1625@users.noreply.github.com>
2025-04-29 14:39:37 +0530

    office

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
-
+window.addEventListener('scroll', function () {
+    const parallax = document.querySelector('.parallax-bg');
+    const scrolled = window.scrollY;
+    
+    // Adjust the rate here (e.g., 0.4 for subtle parallax)
+    parallax.style.transform = `translateY(${scrolled * 0.4}px)`;
+  });
